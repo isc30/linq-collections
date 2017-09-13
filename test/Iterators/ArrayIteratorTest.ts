40e530a95288d790bc794e8d357845465711d42f
Ivan Sanz-Carasa <ivansanzcarasa@gmail.com>
2017-09-13 11:20:23 +0200

    Improve iterator tests

--- test/Iterators/ArrayIteratorTest.ts
+++ test/Iterators/ArrayIteratorTest.ts
@@ -5,38 +5,155 @@ export namespace ArrayIteratorTests
 {
     export function run(): void
     {
-        it("Reset, Next, Value", general);
-        it("Clone", clone);
+        describe("Next", next);
+        describe("Reset", reset);
+        describe("Value", value);
+        describe("Clone", clone);
     }
 
-    function general(): void
+    function next(): void
     {
-        let i = new ArrayIterator<number>([]);
-        Test.isFalse(i.next());
-        Test.throwsException(() => i.value());
+        it("Return false for empty collection", () =>
+        {
+            const it = new ArrayIterator<number>([]);
+            Test.isFalse(it.next());
+        });
 
-        i = new ArrayIterator<number>([2, 4, 6]);
-        Test.isTrue(i.next());
-        Test.isEqual(i.value(), 2);
-        Test.isTrue(i.next());
-        Test.isEqual(i.value(), 4);
-        Test.isTrue(i.next());
-        Test.isEqual(i.value(), 6);
-        Test.isFalse(i.next());
-        Test.throwsException(() => i.value());
+        it("Iterate over elements + return false in the end", () =>
+        {
+            const it = new ArrayIterator<number>([1, 2, 3]);
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isTrue(it.next()); // 3
+            Test.isFalse(it.next());
+        });
+    }
+
+    function reset(): void
+    {
+        it("Iterator is resetted correctly", () =>
+        {
+            const it = new ArrayIterator<number>([1, 2]);
+
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isFalse(it.next());
+            it.reset();
+
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isFalse(it.next());
+            it.reset();
+
+            Test.isTrue(it.next()); // 1
+            it.reset();
+
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isFalse(it.next());
+        });
+
+        it("Multiple reset in a row act like single one", () =>
+        {
+            const it = new ArrayIterator<number>([1, 2]);
+
+            it.reset();
+            it.reset();
+            it.reset();
+
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isFalse(it.next());
+
+            it.reset();
+            it.reset();
+            it.reset();
+
+            Test.isTrue(it.next()); // 1
+            Test.isTrue(it.next()); // 2
+            Test.isFalse(it.next());
+
+            it.reset();
+            it.reset();
+            it.reset();
+        });
+    }
+
+    function value(): void
+    {
+        it("Exception if getting an out of bounds value", () =>
+        {
+            const it = new ArrayIterator<number>([]);
+            Test.isFalse(it.next());
+            Test.throwsException(() => it.value());
+        });
+
+        it("Get values", () =>
+        {
+            const it = new ArrayIterator<number>([2, 4, 6]);
+
+            Test.isTrue(it.next()); Test.isEqual(it.value(), 2);
+            Test.isTrue(it.next()); Test.isEqual(it.value(), 4);
+            Test.isTrue(it.next()); Test.isEqual(it.value(), 6);
+            Test.isFalse(it.next());
+        });
+
+        it("Get values + exception in the end (out of bounds)", () =>
+        {
+            const it = new ArrayIterator<number>([2, 4]);
+
+            Test.isTrue(it.next()); Test.isEqual(it.value(), 2);
+            Test.isTrue(it.next()); Test.isEqual(it.value(), 4);
+            Test.isFalse(it.next());
+
+            Test.throwsException(() => it.value());
+        });
     }
 
     function clone(): void
     {
-        const i = new ArrayIterator<number>([2, 4, 6]);
-        Test.isTrue(i.next());
-        Test.isEqual(i.value(), 2);
+        it("Cloned iterator is resetted by default", () =>
+        {
+            const original = new ArrayIterator<number>([2, 4, 6]);
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 2);
 
-        const x = i.clone();
-        Test.isTrue(i.next());
-        Test.isEqual(i.value(), 4);
+            const clone = original.clone();
+            Test.isTrue(clone.next()); Test.isEqual(clone.value(), 2);
+        });
 
-        Test.isTrue(x.next());
-        Test.isEqual(x.value(), 2);
+        it("Cloned iterator doesn't affect original one", () =>
+        {
+            const original = new ArrayIterator<number>([2, 4, 6]);
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 2);
+
+            const cloned = original.clone();
+            cloned.next(); // 2
+            cloned.next(); // 4
+
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 4);
+
+            cloned.reset();
+
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 6);
+            Test.isFalse(original.next());
+        });
+
+        it("Cloned iterator is identical to original", () =>
+        {
+            const original = new ArrayIterator<number>([2, 4, 6]);
+            const cloned = original.clone();
+
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 2);
+            Test.isTrue(cloned.next()); Test.isEqual(cloned.value(), 2);
+
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 4);
+            Test.isTrue(cloned.next()); Test.isEqual(cloned.value(), 4);
+
+            Test.isTrue(original.next()); Test.isEqual(original.value(), 6);
+            Test.isTrue(cloned.next()); Test.isEqual(cloned.value(), 6);
+
+            Test.isFalse(original.next());
+            Test.isFalse(cloned.next());
+        });
     }
 }
--- test/Linq/IEnumerableTests.ts
+++ test/Linq/IEnumerableTests.ts
@@ -6,7 +6,8 @@ export namespace IEnumerableTests
 {
     export function run(): void
     {
-        it("ToArray", toArray);
+        describe("ToArray", toArray);
+        describe("Aggregate", aggregate);
     }
 
     function toArray(): void
@@ -35,5 +36,36 @@ export namespace IEnumerableTests
         const str = "asdasdsad";
         const strI2 = Enumerable.fromSource(new StringIterator(str));
         Test.isArrayEqual(strI2.toArray(), str.split(""));
+    }
+
+    function aggregate(): void
+    {
+        it("Exception if empty", () =>
+        {
+            const base = Enumerable.fromSource<number>([]);
+            Test.throwsException(() => base.aggregate((p, c) => c));
+        });
+
+        it("No initial value", () =>
+        {
+            const base = Enumerable.fromSource(["a", "b", "a", "a"]);
+            Test.isEqual(base.aggregate((p, c) => p === "b" ? p : c), "b");
+            Test.isEqual(base.aggregate((p, c) => p !== undefined ? c + p : c), "aaba");
+        });
+
+        it("Custom initial value", () =>
+        {
+            const base = Enumerable.fromSource(["a", "b", "a", "a"]);
+            Test.isEqual(base.aggregate((p, c) => p !== undefined ? c + p : c, "xd"), "aabaxd");
+        });
+
+        it("Custom return type", () =>
+        {
+            const base = Enumerable.fromSource(["a", "b", "a", "a"]);
+            Test.isTrue(base.aggregate((p, c) => p || c === "b", false));
+            Test.isTrue(base.aggregate((p, c) => p || c === "a", false));
+            Test.isFalse(base.aggregate((p, c) => p || c === "x", false));
+            Test.isTrue(base.aggregate((p, c) => p || c === "x", true));
+        });
     }
 }
