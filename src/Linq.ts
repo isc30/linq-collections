b8b5ed2fd84437ded99c0bde3cfc4b66296323b6
Ivan Sanz-Carasa <ivansanzcarasa@gmail.com>
2018-04-07 23:23:27 +0200

    [#26] Webpack + gulp + browser version

--- src/Linq.ts
+++ src/Linq.ts
@@ -3,8 +3,22 @@
  * Copyright © 2017 Ivan Sanz Carasa. All rights reserved.
 */
 
+// Node
 export { IQueryable, IKeyValue, IGrouping, IEnumerable, IOrderedEnumerable, Enumerable } from "./Enumerables";
 export { IIterable, ArrayIterator } from "./Iterators";
 export { IReadOnlyList, IList, List, IReadOnlyDictionary, IDictionary, Dictionary, IStack, Stack } from "./Collections";
 export { Comparer, ComparerResult } from './Comparers';
 export * from './Types';
+
+// Browser
+import { Enumerable } from "./Enumerables";
+import { ArrayIterator } from "./Iterators";
+import { List, Dictionary, Stack } from "./Collections";
+
+declare const window: any;
+
+window.LinqCollections = {
+    Enumerable,
+    ArrayIterator,
+    List, Dictionary, Stack
+};
