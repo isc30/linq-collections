d4cc24798d1c8511841f8e33d5723739daf604e8
Ivan Sanz-Carasa <ivansanzcarasa@gmail.com>
2018-04-19 14:28:55 +0200

    latest changes

--- src/Linq.ts
+++ src/Linq.ts
@@ -3,23 +3,8 @@
  * Copyright © 2017 Ivan Sanz Carasa. All rights reserved.
 */
 
-// Node
 export { IQueryable, IKeyValue, IGrouping, IEnumerable, IOrderedEnumerable, Enumerable } from "./Enumerables";
 export { IIterable } from "./Iterators";
 export { IReadOnlyList, IList, List, IReadOnlyDictionary, IDictionary, Dictionary, IStack, Stack } from "./Collections";
 export { Comparer, ComparerResult, EqualityComparer, strictEqualityComparer } from './Comparers';
 export * from './Types';
-
-// Browser
-import { Enumerable } from "./Enumerables";
-import { List, Dictionary, Stack } from "./Collections";
-
-declare const window: any;
-
-if (window !== undefined)
-{
-    Object.freeze(window.Enumerable = Enumerable);
-    Object.freeze(window.List = List);
-    Object.freeze(window.Dictionary = Dictionary);
-    Object.freeze(window.Stack = Stack);
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const dist = {
+const dist_node = {
   mode: "production",
   entry: {
     'linq-collections': './src/Linq.ts'
@@ -18,15 +18,16 @@ const dist = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
+    libraryTarget: "commonjs",
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   }
 };
 
-const tests = {
+const dist_umd = {
   mode: "production",
   entry: {
-    'linq-collections-tests': './test/TestSuite.ts'
+    'linq-collections-umd': './src/Linq.ts'
   },
   module: {
     rules: [
@@ -41,9 +42,35 @@ const tests = {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
   output: {
+    libraryTarget: "umd",
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+const tests = {
+  mode: "development",
+  devtool: "source-map",
+  entry: {
+    'linq-collections-test': './test/TestSuite.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    libraryTarget: "commonjs",
     filename: '[name].js',
     path: path.resolve(__dirname, '')
   }
 };
 
-module.exports = [ dist, tests ];
+module.exports = [ dist_node, dist_umd, tests ];
