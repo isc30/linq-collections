4775f3877486d7bd13f314010fcdb42eea7a7472
Łukasz Wójcik <arbor@o2.pl>
2018-04-19 22:46:43 +0200

    init webpack

--- src/Linq.ts
+++ src/Linq.ts
@@ -6,5 +6,5 @@
 export { IQueryable, IKeyValue, IGrouping, IEnumerable, IOrderedEnumerable, Enumerable } from "./Enumerables";
 export { IIterable, ArrayIterator } from "./Iterators";
 export { IReadOnlyList, IList, List, IReadOnlyDictionary, IDictionary, Dictionary, IStack, Stack } from "./Collections";
-export { Comparer, ComparerResult, EqualityComparer, strictEqualityComparer } from './Comparers';
-export * from './Types';
+export { Comparer, ComparerResult, EqualityComparer, strictEqualityComparer } from "./Comparers";
+export * from "./Types";
