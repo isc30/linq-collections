020e9cad94a05a5d95f7a5acf977fdfd47e51b33
Ivan Sanz Carasa <ivansanzcarasa@gmail.com>
2018-12-28 09:32:39 +0100

    fix lint

--- test/Iterators/ArrayIterator.test.ts
+++ test/Iterators/ArrayIterator.test.ts
@@ -1,4 +1,4 @@
-import { run as runIteratorTests } from "./Iterator.spec";
 import { ArrayIterator } from "../../src/Iterators";
+import { run as runIteratorTests } from "./Iterator.spec";
 
 runIteratorTests(<T>(e: T[]) => new ArrayIterator<T>(e));
